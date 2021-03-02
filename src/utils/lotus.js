2adb3bbf15f575e3b5d0615929b76e2e7aa741b4
Mike Seese <seesemichaelj@gmail.com>
2021-03-02 14:36:39 -0800

    change hardcoded websocket url to assume single lotus node

--- src/utils/lotus.js
+++ src/utils/lotus.js
@@ -7,7 +7,7 @@ export const getClient = (options = { nodeOrMiner: "node", nodeNumber: 0 }) => {
   const API = "ws://localhost:7777";
 
   // Websocket endpoint for local Lotus devnet
-  const wsUrl = API + `/${options.nodeNumber}/${options.nodeOrMiner}/rpc/v0`;
+  const wsUrl = API + `/rpc/v0`;
 
   // Creating and returning a Lotus client that can be used anywhere in the app
   const provider = new BrowserProvider(wsUrl);
